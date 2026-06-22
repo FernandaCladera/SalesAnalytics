d6d109efcf24cc7d9982096c12ae121cda26710c
Fernanda Cladera Melgar <fercladeram@gmail.com>
2026-06-22 19:04:36 +0200

    clean code, eliminate testing with docling

--- Scripts/docling_try.py
+++
@@ -1,6 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-source = "../Reports/240312_Tecan_Presentation_FYR-2023.pdf"
-converter = DocumentConverter()
-doc = converter.convert(source).document
-print(doc.export_to_markdown())
